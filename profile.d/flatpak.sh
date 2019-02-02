bef3461da2bd6337f29d82aa26211178e17a5db6
femij <femij@femi-pc>
2019-02-02 13:39:19 -0500

    committing changes in /etc made by "sudo pacman -Syu"
    Package changes:

--- profile.d/flatpak.sh
+++ profile.d/flatpak.sh
@@ -1,7 +1,23 @@
-# /etc/profile.d/flatpak.sh - set XDG_DATA_DIRS
+# set XDG_DATA_DIRS to include Flatpak installations
 
-if [ "${XDG_DATA_DIRS#*flatpak}" = "${XDG_DATA_DIRS}" ]; then
-    XDG_DATA_DIRS="${XDG_DATA_HOME:-"$HOME/.local/share"}/flatpak/exports/share:/var/lib/flatpak/exports/share:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
-fi
+new_dirs=$(
+    (
+        echo "${XDG_DATA_HOME:-"$HOME/.local/share"}/flatpak"
+        flatpak --installations
+    ) | (
+        new_dirs=
+        while read -r install_path
+        do
+            share_path=$install_path/exports/share
+            case ":$XDG_DATA_DIRS:" in
+                *":$share_path:"*) :;;
+                *":$share_path/:"*) :;;
+                *) new_dirs=${new_dirs:+${new_dirs}:}$share_path;;
+            esac
+        done
+        echo "$new_dirs"
+    )
+)
 
 export XDG_DATA_DIRS
+XDG_DATA_DIRS="${new_dirs:+${new_dirs}:}${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
